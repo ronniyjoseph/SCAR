27c0a3361b880d20b753217280e3bd767bb90131
Ronniy Joseph <ronniyjoseph@gmail.com>
2018-10-16 13:30:23 +0800

    Delete bashed_cramps.sh

--- bashed_cramps.sh
+++
@@ -1,3 +0,0 @@
-
-python CRAMPS.py -path "/home/rjoseph/Bulk/Redundant_Calibration/Simulation_Output/CRAMPS_Hex_ideal_P100_BG_fullcal/" -MP -pointJy 100 -cal_mode 'full'
-python CRAMPS.py -path "/home/rjoseph/Bulk/Redundant_Calibration/Simulation_Output/CRAMPS_Hex_offset_P100_BG_fullcal/" -MP -pointJy 100 -offset -cal_mode 'full'
